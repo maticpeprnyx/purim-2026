dcf28cf01bb3cbcab5e86eea75d1715e61ad124b
matamatic <paloudovajosefina@gmail.com>
2026-02-11 19:02:53 +0100

    Ozzy a Janička Uriel

---
+++ data.js
@@ -0,0 +1,50 @@
+const entities = [
+    {
+        name: "Ozzy Osbourne",
+        label: "Zpěvák metalové skupiny Black Sabbath",
+        images: [
+                    {
+                        url: "https://www.rollingstone.com/wp-content/uploads/2002/07/GettyImages-2887285.jpg",
+                        alt: "Ozzy Osbourne s charakteristickými kulatými brýlemi"
+                    },
+                    {
+                        url: "https://i.abcnewsfe.com/a/75838cd7-1099-4c6a-8475-3b7e60e2e2f1/black-sabbath-gty-gmh-250205_1738786910005_hpMain.jpg",
+                        alt: "Kapela Black Sabbath (Ozzy Osbourne vlevo)"
+                    },
+                    {
+                        url: "https://www.actingthepart.com.au/wp-content/uploads/2023/03/Ozzy-Osbourne.jpg",
+                        alt: "Jedno z možných pojetí kostýmu"
+                    }
+                ],
+        performanceUrl: "https://youtu.be/zqkKzZheXQg?si=S9AFpxU5XLqfBLJ0&t=335",
+        musicUrl: "https://youtu.be/PrZFscfJxXc?si=4bGmJD0c0FjfdHQq",
+        comment: "Relevantní pochopitelně spíše pro starší generace. Kontroverzní postava kvůli drogám a obecně životnímu stylu, ale významná osobnost pro metal, ale i rockovou muziku vůbec. Na hudbu Black Sabbath má cenu upozorňovat."
+    },
+    {
+        name: "Jana Kratochvílová",
+        label: "Česká textařka a zpěvačka původně spojená s Kroky Františka Janečka, která ale zaznamenala i po revoluci úspěšnou sólo dráhu",
+        images: [
+            {
+                url: "https://www.play.cz/wp-content/uploads/2023/01/Jana-Uriel-Kratochvilova.jpg",
+                alt: "Jana Kratochvílová počátkem devadesátých let"
+            },
+            {
+                url: "https://festival.praguepride.com/app/uploads/2025/06/foto-kratochvilova-1920x1080-c-default.jpg",
+                alt: "Jana Uriel Kratochvílová na oslavě svých 70. narozenin (2023)"
+            },
+            {
+                url: "https://cdn.xsd.cz/original/094d509f7cce3fd1a9950b29a5eda457.jpg",
+                alt: "Jana Uriel Kratochvílová pózující v typicky výstředním outfitu"
+            }
+        ],
+        performanceUrl: "https://youtu.be/s1Vr2RacZkU?si=Y7n54Rb2PILJy40J&t=77",
+        musicUrl: "https://youtu.be/oZbiqpYE3i8?si=MSfP0T3pDoWl9WAP",
+        comment: "Zaprvý, ta hudba má grády a živé koncerty svým formátem dost úspěšně drží krok s dobou. Janička Uriel sama je známá jako poměrně trhlá bába s výstředním způsobem oblíkání, vyjadřování i uvažování. Podle mě královna, ale mladší ročníky ji asi moc znát nebudou."
+    },
+
+
+        
+
+
+
+]
