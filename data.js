5b59af6f2827ab1912bd2f3037d6eb6c37b30031
matamatic <paloudovajosefina@gmail.com>
2026-02-12 10:18:15 +0100

    Daft Punk

--- data.js
+++ data.js
@@ -2,27 +2,35 @@ const entities = [
     {
         name: "Ozzy Osbourne",
         label: "Zpěvák metalové skupiny Black Sabbath",
+        category: [
+            "skupina",
+            "solo"
+        ],
         images: [
-                    {
-                        url: "https://www.rollingstone.com/wp-content/uploads/2002/07/GettyImages-2887285.jpg",
-                        alt: "Ozzy Osbourne s charakteristickými kulatými brýlemi"
-                    },
-                    {
-                        url: "https://i.abcnewsfe.com/a/75838cd7-1099-4c6a-8475-3b7e60e2e2f1/black-sabbath-gty-gmh-250205_1738786910005_hpMain.jpg",
-                        alt: "Kapela Black Sabbath (Ozzy Osbourne vlevo)"
-                    },
-                    {
-                        url: "https://www.actingthepart.com.au/wp-content/uploads/2023/03/Ozzy-Osbourne.jpg",
-                        alt: "Jedno z možných pojetí kostýmu"
-                    }
-                ],
+            {
+                url: "https://www.rollingstone.com/wp-content/uploads/2002/07/GettyImages-2887285.jpg",
+                alt: "Ozzy Osbourne s charakteristickými kulatými brýlemi"
+            },
+            {
+                url: "https://i.abcnewsfe.com/a/75838cd7-1099-4c6a-8475-3b7e60e2e2f1/black-sabbath-gty-gmh-250205_1738786910005_hpMain.jpg",
+                alt: "Kapela Black Sabbath (Ozzy Osbourne vlevo)"
+            },
+            {
+                url: "https://www.actingthepart.com.au/wp-content/uploads/2023/03/Ozzy-Osbourne.jpg",
+                alt: "Jedno z možných pojetí kostýmu"
+            }
+        ],
         performanceUrl: "https://youtu.be/zqkKzZheXQg?si=S9AFpxU5XLqfBLJ0&t=335",
         musicUrl: "https://youtu.be/PrZFscfJxXc?si=4bGmJD0c0FjfdHQq",
-        comment: "Relevantní pochopitelně spíše pro starší generace. Kontroverzní postava kvůli drogám a obecně životnímu stylu, ale významná osobnost pro metal, ale i rockovou muziku vůbec. Na hudbu Black Sabbath má cenu upozorňovat."
+        comment: {
+            author: "Josefínin",
+            content: "Relevantní pochopitelně spíše pro starší generace. Kontroverzní postava kvůli drogám a obecně životnímu stylu, ale významná osobnost pro metal, ale i rockovou muziku vůbec. Na hudbu Black Sabbath má cenu upozorňovat."
+        }
     },
     {
         name: "Jana Kratochvílová",
         label: "Česká textařka a zpěvačka původně spojená s Kroky Františka Janečka, která ale zaznamenala i po revoluci úspěšnou sólo dráhu",
+        category: "solo",
         images: [
             {
                 url: "https://www.play.cz/wp-content/uploads/2023/01/Jana-Uriel-Kratochvilova.jpg",
@@ -39,12 +47,58 @@ const entities = [
         ],
         performanceUrl: "https://youtu.be/s1Vr2RacZkU?si=Y7n54Rb2PILJy40J&t=77",
         musicUrl: "https://youtu.be/oZbiqpYE3i8?si=MSfP0T3pDoWl9WAP",
-        comment: "Zaprvý, ta hudba má grády a živé koncerty svým formátem dost úspěšně drží krok s dobou. Janička Uriel sama je známá jako poměrně trhlá bába s výstředním způsobem oblíkání, vyjadřování i uvažování. Podle mě královna, ale mladší ročníky ji asi moc znát nebudou."
+        comment: {
+            author: "Josefínin",
+            content: "Zaprvý, ta hudba má grády a živé koncerty svým formátem dost úspěšně drží krok s dobou. Janička Uriel sama je známá jako poměrně trhlá bába s výstředním způsobem oblíkání, vyjadřování i uvažování. Podle mě královna, ale mladší ročníky ji asi moc znát nebudou."
+        }
     },
-
-
-        
-
-
-
+    {
+        name: "Daft Punk",
+        label: "Elektronické duo z Francie",
+        category: "skupina",
+        images: [
+            {
+                url: "https://static01.nyt.com/images/2021/02/23/arts/22daftpunk/merlin_68436082_cbc2d16f-e4ce-4e8e-9f06-d2789c9f309b-superJumbo.jpg",
+                alt: "Daft Punk v jejich charakteristických pochromovaných helmách"
+            },
+            {
+                url: "https://cached.imagescaler.hbpl.co.uk/resize/scaleHeight/815/cached.offlinehbpl.hbpl.co.uk/news/OMC/DAFTPUNK.jpg",
+                alt: "Daft Punk na veřejnosti stále nosí své helmy"
+            },
+            {
+                url: "https://www.partiescostume.com/wp-content/uploads/2015/11/Daft-Punk-Halloween-Costumes.jpg",
+                alt: "Možné pojetí kostýmu"
+            },
+        ],
+        performanceUrl: "https://youtu.be/BMC8nTo5oJo?si=IMrjuO5X5ZOX2l3X",
+        musicUrl: "https://youtu.be/gAjR4_CbPpQ?si=7bkJ3CIOv121N_D8",
+        comment: {
+            author: "Josefínin",
+            content: "Ideální volba pro ty, kteří chtějí schovat obličej a zároveň se vydávat za rozpoznatelné hudebníky. Daft Punk v rámci elektronické hudby klasika a vzhledem k tomu, že se rozhodli ukončit kariéru teprv před třemi lety, jejich hudbu znají i mladší ročníky. Navíc mají velmi odmítavý postoj k umělé inteligenci, což by podle mých zkušeností mohlo mezi studenty rezonovat."
+        }
+    },
+    {
+        name: "",
+        label: "",
+        category: "",
+        images: [
+            {
+                url: "",
+                alt: ""
+            },
+            {
+                url: "",
+                alt: ""
+            },{
+                url: "",
+                alt: ""
+            }
+        ],
+        performanceUrl: "",
+        musicUrl: "",
+        comment: {
+            author: "",
+            content: ""
+        }
+    }
 ]
